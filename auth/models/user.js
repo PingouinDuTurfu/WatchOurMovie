220b554e8b8c15796fdc876e1ebda5e3e9f8ab19
ollivier remy <remy.olliver@gmail.com>
2024-03-22 11:55:37 +0100

    Add auth provider

---
+++ auth/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const URL = process.env.MONGO_URL || 'localhost';
+const PORT = process.env.MONGO_PORT || '27017';
+
+const USER = process.env.MONGO_USER || '';
+const PASSWORD = process.env.MONGO_PASSWORD || '';
+const DB = process.env.MONGO_AUTH_DB || 'default';
+
+if (!USER || !PASSWORD) {
+    console.error('MongoDB environment variables not set');
+    process.exit(1);
+}
+
+mongoose.connect(`mongodb://${URL}:${PORT}`, {
+    user: USER,
+    pass: PASSWORD,
+    dbName: DB,
+})
+    .then(() => console.log('Connected!'))
+    .catch(err => console.log(err));
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', userSchema);
