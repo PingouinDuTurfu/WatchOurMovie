772d914e47f8c3b7d537fa7edb89a9e9f676850e
eiliel <ameilie@hotmail.fr>
2024-04-05 23:38:56 +0200

    Connexion

--- front/wom/src/components/AppConnexion.tsx
+++ front/wom/src/components/AppConnexion.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from "../css/AppConnexion.module.css";
+import { Button, Paper, TextField } from '@mui/material';
 
 export default function AppConnexion() {
   return (
-    <div>AppConnexion</div>
-  )
+    <div className={styles.container}>
+      <Paper elevation={3} className={styles.paperConnexion}>
+        <div className={styles.connexionContainer}>
+        <h2>Connexion</h2>
+        <TextField className={styles.inputText} id="pseudo" label="Pseudo" variant="outlined" fullWidth />
+        <TextField className={styles.inputText} id="password" label="Mot de passe" type="password" variant="outlined" fullWidth />
+        <Button variant="contained" color="primary" fullWidth>
+          Se connecter
+        </Button></div>
+        <div className={styles.inscriptionContainer}>
+        <h2>Pas encore inscrit ?</h2>
+        <p>Inscrivez-vous dès maintenant !</p>
+        <Button component={Link} to="/inscription" variant="outlined" fullWidth>
+          Inscription
+        </Button></div>
+      </Paper>
+    </div>
+  );
 }
