b9c2e6618227183a53a2eec3ff887441739adff0
ollivier remy <remy.olliver@gmail.com>
2024-04-06 16:06:10 +0200

    Setup docker

--- db/app.js
+++ db/app.js
@@ -8,7 +8,7 @@ const movieRoutes = require('./routes/movieRoute');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_MANAGER_PORT || 3000;
 
 initialize();
 
