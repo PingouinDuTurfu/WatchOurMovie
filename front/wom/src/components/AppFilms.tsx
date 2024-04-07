4576d44f1152099b34ec1bd934d7ee4bb871fa48
eiliel <ameilie@hotmail.fr>
2024-04-07 18:23:23 +0200

    Search movies & apiUtils

--- front/wom/src/components/AppFilms.tsx
+++ front/wom/src/components/AppFilms.tsx
@@ -1,57 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styles from "../css/AppFilms.module.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { IconButton, InputBase } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 const filmsData = [
   {
     id: 1,
-    title: 'Dune 2',
-    imageUrl: 'dune2.jpg'
+    title: "Dune 2",
+    imageUrl: "dune2.jpg",
   },
   {
     id: 2,
-    title: 'Dune 2',
-    imageUrl: 'dune2.jpg'
+    title: "Dune 2",
+    imageUrl: "dune2.jpg",
   },
   {
     id: 3,
-    title: 'Dune 2',
-    imageUrl: 'dune2.jpg'
+    title: "Dune 2",
+    imageUrl: "dune2.jpg",
   },
   {
     id: 4,
-    title: 'Dune 2',
-    imageUrl: 'dune2.jpg'
+    title: "Dune 2",
+    imageUrl: "dune2.jpg",
   },
   {
     id: 5,
-    title: 'Dune 2',
-    imageUrl: 'dune2.jpg'
+    title: "Dune 2",
+    imageUrl: "dune2.jpg",
   },
   {
     id: 6,
-    title: 'Dune 2',
-    imageUrl: 'dune2.jpg'
+    title: "Dune 2",
+    imageUrl: "dune2.jpg",
   },
   {
     id: 7,
-    title: 'Dune 2',
-    imageUrl: 'dune2.jpg'
+    title: "Dune 2",
+    imageUrl: "dune2.jpg",
   },
   {
     id: 8,
-    title: 'Dune 2',
-    imageUrl: 'dune2.jpg'
+    title: "Dune 2",
+    imageUrl: "dune2.jpg",
   },
   {
     id: 9,
-    title: 'Dune 2',
-    imageUrl: 'dune2.jpg',
+    title: "Dune 2",
+    imageUrl: "dune2.jpg",
   },
   {
     id: 10,
-    title: 'Dune 2',
-    imageUrl: 'dune2.jpg',
+    title: "Dune 2",
+    imageUrl: "dune2.jpg",
+  },
+  {
+    id: 11,
+    title: "Godzilla vs Kong",
+    imageUrl: "godzillakong.jpg",
   },
 ];
 
@@ -59,19 +66,25 @@ const filmsPerPage = 6;
 
 export default function AppFilms() {
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchValue, setSearchValue] = useState("");
 
   const startIndex = (currentPage - 1) * filmsPerPage;
   const endIndex = startIndex + filmsPerPage;
-  const filmsToShow = filmsData.slice(startIndex, endIndex);
 
-  const totalPages = Math.ceil(filmsData.length / filmsPerPage);
+  const filteredFilms = filmsData.filter((film) =>
+    film.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  const filmsToShow = filteredFilms.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(filteredFilms.length / filmsPerPage);
 
   const paginationSquares = [];
   for (let i = 1; i <= totalPages; i++) {
     paginationSquares.push(
-      <button 
-        key={i} 
-        onClick={() => setCurrentPage(i)} 
+      <button
+        key={i}
+        onClick={() => setCurrentPage(i)}
         className={currentPage === i ? styles.activePage : styles.pageSquare}
       >
         {i}
@@ -79,26 +92,44 @@ export default function AppFilms() {
     );
   }
 
+  function handleSearchEntry(searchEntry: string) {
+    setSearchValue(searchEntry);
+    setCurrentPage(1);
+  }
+
   return (
     <div className={styles.container}>
       <h1>Films</h1>
+      <div className={styles.searchBar}>
+        <InputBase
+          placeholder="Rechercher…"
+          value={searchValue}
+          onChange={(e) => handleSearchEntry(e.target.value)}
+          className={styles.searchInput}
+        />
+        <IconButton type="submit" aria-label="search">
+          <Search />
+        </IconButton>
+      </div>
+
       <div className={styles.filmsContainer}>
         {filmsToShow.map((film) => (
-          <Link to={`/films/${film.id}`} key={film.id} className={styles.filmContainer}>
-            <img 
-              src={film.imageUrl} 
-              alt={film.title} 
-              className={styles.filmImage} 
-              style={{ width: '100%' }} 
+          <Link
+            to={`/films/${film.id}`}
+            key={film.id}
+            className={styles.filmContainer}
+          >
+            <img
+              src={film.imageUrl}
+              alt={film.title}
+              className={styles.filmImage}
+              style={{ width: "100%" }}
             />
             <div className={styles.title}>{film.title}</div>
           </Link>
         ))}
       </div>
-      <div className={styles.pagination}>
-        {paginationSquares}
-      </div>
+      <div className={styles.pagination}>{paginationSquares}</div>
     </div>
   );
 }
-
---
+++ front/wom/src/utils/ApiUtils.ts
@@ -0,0 +1,64 @@
+import axios, { AxiosInstance } from "axios";
+
+export default abstract class ApiUtils {
+  private static AUTH_TOKEN: string | null = null;
+  private static readonly API_BASE_URL = "http://localhost:4269";
+  private static readonly API_INSTANCE_JSON = axios.create({
+    baseURL: ApiUtils.API_BASE_URL,
+    headers: {
+      "Content-Type": "multipart/json",
+    },
+  });
+  private static readonly API_INSTANCE_FORM_DATA = axios.create({
+    baseURL: ApiUtils.API_BASE_URL,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  static getApiInstanceJson(): AxiosInstance {
+    return ApiUtils.API_INSTANCE_JSON;
+  }
+
+  static getApiInstanceFormData(): AxiosInstance {
+    return ApiUtils.API_INSTANCE_FORM_DATA;
+  }
+
+  static async login(
+    username: string,
+    password: string
+  ): Promise<string | null> {
+    try {
+      const response = await ApiUtils.API_INSTANCE_JSON.post("/login", {
+        username,
+        password,
+      });
+      const token = response.data.token;
+      ApiUtils.AUTH_TOKEN = token;
+      return token;
+    } catch (error) {
+      console.error("Error logging in:", error);
+      return null;
+    }
+  }
+
+  static async logout(): Promise<void> {
+    return ApiUtils.API_INSTANCE_JSON.post("/logout", {
+      token: ApiUtils.AUTH_TOKEN,
+    })
+      .then(() => {
+        ApiUtils.AUTH_TOKEN = null;
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la déconnexion :", error);
+      });
+  }
+
+  static getAuthToken(): string | null {
+    return ApiUtils.AUTH_TOKEN;
+  }
+
+  static setAuthToken(token: string): void {
+    ApiUtils.AUTH_TOKEN = token;
+  }
+}
