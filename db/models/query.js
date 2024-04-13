905f3be0432281c0dc7dc6ac1a4f3570e5ca154d
ollivier remy <remy.olliver@gmail.com>
2024-04-13 16:17:39 +0200

    Add search route

---
+++ db/models/query.js
@@ -0,0 +1,30 @@
+const database = require('../database/database');
+
+const querySchema = new database.Schema({
+    request: {
+        include_adult: { type: Boolean },
+        language: { type: String },
+        page: { type: Number },
+        region: { type: String },
+        query: { type: String, required: true },
+    },
+    response: [
+        {
+            adult: { type: Boolean },
+            genre_ids: { type: [Number] },
+            id: { type: Number, required: true },
+            original_language: { type: String },
+            original_title: { type: String },
+            overview: { type: String },
+            popularity: { type: Number },
+            poster_path: { type: String },
+            release_date: { type: String },
+            title: { type: String },
+            video: { type: Boolean },
+            vote_average: { type: Number },
+            vote_count: { type: Number }
+        }
+    ]
+});
+
+module.exports = database.model('Query', querySchema);
