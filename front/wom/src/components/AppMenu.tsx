a896f181cbe6a69cc176444d6e20f84d6c31d5c4
eiliel <ameilie@hotmail.fr>
2024-04-14 20:41:55 +0200

    package & search

--- front/wom/src/components/AppMenu.tsx
+++ front/wom/src/components/AppMenu.tsx
@@ -1,18 +1,14 @@
-import { Search } from '@mui/icons-material'
 import { AppBar, Button, IconButton, TextField, Toolbar, Typography } from '@mui/material'
 import styles from "../css/AppMenu.module.css";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../auth/AuthProvider';
-import { useState } from 'react';
 
 interface AppMenuProps {
   setFooterNavValue: React.Dispatch<React.SetStateAction<number | undefined>>;
 }
 
 export default function AppMenu({ setFooterNavValue }: AppMenuProps) {
-  const [searchValue, setSearchValue] = useState<string>("");
   const { authToken, logout } = useAuth();
-  const navigate = useNavigate();
 
   function handleNavClick(value: number | undefined) {
     setFooterNavValue(value);
@@ -22,32 +18,12 @@ export default function AppMenu({ setFooterNavValue }: AppMenuProps) {
     logout();
   }
 
-  function handleSearchClick() {
-    navigate(`/films/recherche/${searchValue}`);
-  }
-
-  function handleKeyPress(event: React.KeyboardEvent<HTMLDivElement>) {
-    if (event.key === "Enter") {
-      handleSearchClick();
-    }
-  }
-
   return (
     <AppBar className={styles.navMenu}>
       <Toolbar>
         <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
           Watch Our Movie
         </Typography>
-        <div>
-          <TextField
-            className={styles.searchBar}
-            placeholder="Rechercher un film…"
-            onKeyDown={handleKeyPress}
-          />
-          <IconButton type="submit" aria-label="search" onClick={() => handleSearchClick()}>
-            <Search/>
-          </IconButton>
-        </div>
         <Button color="inherit" component={Link} to="/" onClick={() => handleNavClick(0)}>
           Accueil
         </Button>
