d868e22dcbd80c435ac065ede511b6a53bd130c3
Axel LANTA <lantaaxel@gmail.com>
2024-04-14 15:28:28 +0200

    Fix recommendation

--- api/routers/group.py
+++ api/routers/group.py
@@ -120,7 +120,7 @@ def get_group_recommendation(language: str, groupName: str, onMoviesSeen: bool,
         recommendations = []
         recommended_movie_ids = set()
 
-        if movies_seen and not onMoviesSeen:
+        if movies_seen and onMoviesSeen:
             recommendations, recommended_movie_ids = recommendation_with_movie(most_seen_movies, language, movies_seen,
                                                                                recommended_movie_ids, recommendations,
                                                                                most_common_genre_ids)
@@ -129,7 +129,7 @@ def get_group_recommendation(language: str, groupName: str, onMoviesSeen: bool,
                                                                                   recommended_movie_ids,
                                                                                   recommendations,
                                                                                   most_common_genre_ids)
-
+        print(recommendations)
         sorted_recommendations = sorted(recommendations, key=lambda x: x['popularity'], reverse=True)
         result_reco = []
         genres = get_genres()
@@ -156,7 +156,8 @@ def recommendation_with_movie(most_seen_movies: list, language: str, movies_seen
         raise HTTPException(status_code=404, detail="Language not found")
     for movie_id, _ in most_seen_movies:
         response = requests.post(DB_URL + "/movie/recommendation/",
-                                 json={"movieId": movie_id, "language": language})
+                                 json={"movie_id": movie_id, "language": language, "page": 1})
+        print(response.json())
         if response.status_code == 200 or response.status_code == 201:
             data = response.json()
             for movie in data:
@@ -164,6 +165,7 @@ def recommendation_with_movie(most_seen_movies: list, language: str, movies_seen
                         and any(genre_id in movie['genre_ids'] for genre_id in most_common_genre_ids)):
                     recommendations.append(movie)
                     recommended_movie_ids.add(movie['id'])
+    print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
     return recommendations, recommended_movie_ids
 
 
@@ -182,6 +184,7 @@ def recommendation_without_movie(language: str, movies_seen: set, recommended_mo
                 if movie['id'] not in movies_seen and movie['id'] not in recommended_movie_ids:
                     recommendations.append(movie)
                     recommended_movie_ids.add(movie['id'])
+    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     return recommendations, recommended_movie_ids
 
 
